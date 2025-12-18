f9b560ead8f0a6a0e06464ce04f1fb3514116eb1
Hwangsangha <wsh715@gmail.com>
2025-12-18 17:56:29 +0900

    테스트클래스 수정

--- Shop/src/test/java/com/example/ebook/EbookApplicationTests.java
+++ Shop/src/test/java/com/example/ebook/EbookApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.shop;
+package com.example.ebook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ShopApplicationTests {
+@SpringBootTest(classes = EbookApplication.class)
+class EbookApplicationTests {
 
 	@Test
 	void contextLoads() {
