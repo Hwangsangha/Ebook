fee509e495d42daee1a11ca702b88d38256b3561
Hwangsangha <wsh715@gmail.com>
2025-12-30 15:03:16 +0900

    결제버튼

--- Shop/frontend/src/pages/SummaryPage.jsx
+++ Shop/frontend/src/pages/SummaryPage.jsx
@@ -7,6 +7,7 @@ function SummaryPage(){
     const [summary, setSummary] = useState(null);
     const [error, setError] = useState(null);
     const userId = 1;
+    const [paid, setPaid] = useState(false);
     
     useEffect(() => {
         CartApi.summary(1)
@@ -16,6 +17,22 @@ function SummaryPage(){
 
     if (error) return <p className="ui-muted">에러: {error}</p>;
     if (!summary) return <p className="ui-muted">불러오는 중...</p>;
+
+    if(paid) {
+        return (
+            <div className="ui-page">
+                <Header/>
+                <h1 className="ui-title">결제 완료</h1>
+                <p className="ui-muted">주문이 완료되었습니다.</p>
+
+                <div style={{ display: "flex", justifyContent: "fles-end", marginTop: 12}}>
+                    <button className="ui-btn" onClick={() => setPaid(false)}>
+                        다시 보기
+                    </button>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className="ui-page">
