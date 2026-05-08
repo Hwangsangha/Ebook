f349fcc1e012333020a53d88acf3f73a84f400d0
Hwangsangha <wsh715@gmail.com>
2026-05-08 21:46:27 +0900

    azure배포  환경설정

--- Shop/src/main/java/com/example/ebook/config/WebCorsConfig.java
+++ Shop/src/main/java/com/example/ebook/config/WebCorsConfig.java
@@ -2,6 +2,7 @@ package com.example.ebook.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,6 +20,14 @@ public class WebCorsConfig implements WebMvcConfigurer{
 				.allowCredentials(true)			//쿠키/인증정보 필요 없으면 false
 				.maxAge(3600);						//preflight 캐시(초)
 		
+	}
+
+	//정적 파일(프론트 엔드) 경로 설정
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+				.addResourceLocations("classpath:/static/")
+				.setCachePeriod(0);
 	}
 
 }
